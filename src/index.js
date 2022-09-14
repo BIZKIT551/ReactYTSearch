2d4b7721fa3b9e41240d4f24dcce93e6f4079641
BIZKIT551 <jediaswin333@gmail.com>
2022-09-15 02:24:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyBqz-FKanc-W1ztjpggpRk3Z6uI5LdTM3c";
 
-YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
