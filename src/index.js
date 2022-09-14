5c504fba2aa2e08ecaa3899bf61dbf2a3d0e15e9
BIZKIT551 <jediaswin333@gmail.com>
2022-09-15 02:14:22 +0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyBqz-FKanc-W1ztjpggpRk3Z6uI5LdTM3c";
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
