c1da159aa353492fb48a89fa32d065cdea24b4d1
BIZKIT551 <jediaswin333@gmail.com>
2022-09-18 00:34:23 +0800

    update

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const VideoListItem = ({video}) => {
-    return <li>Video</li>
+const VideoListItem = ({ video }) => {
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-media-object" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading"></div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
